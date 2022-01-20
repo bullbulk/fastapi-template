6ea7200dd7cc96d3deac10fcf40b9e66fda652ee
bullbulk <grur.ura@mail.ru>
2022-01-21 03:58:33 +0500

    basic project structure

---
+++ app/db/init_db.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+
+from app import crud
+from app import schemas
+from app.core.config import settings
+from app.db.base_class import Base
+from app.db.session import engine
+
+
+def init_db(db: Session) -> None:
+    Base.metadata.create_all(bind=engine)
+
+    user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
+    if not user:
+        user_in = schemas.UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = crud.user.create(db, obj_in=user_in)  # noqa: F841
---
+++ app/test.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from core.config import settings
+
+settings.API_V1_STR
+def test_create_item(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    data = {"title": "Foo", "description": "Fighters"}
+    response = client.post(
+        f"localhost:5555/items/", headers=superuser_token_headers, json=data,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["title"] == data["title"]
+    assert content["description"] == data["description"]
+    assert "id" in content
+    assert "owner_id" in content
